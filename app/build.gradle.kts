10335027bb5868cb2ae7af0171922bb3e1a1295b
Yulia Baranova <yulia.gorelikova@yandex.ru>
2024-06-07 22:40:03 +0300

    add picocli

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,6 +17,7 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.11.0-M1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("info.picocli:picocli:4.7.6")
 }
 
 tasks.test {
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,35 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
+import picocli.CommandLine.Option;
+
+import java.util.concurrent.Callable;
+
+@Command (
+        name = "gendiff",
+        description = "Compares two configuration files and shows a difference.",
+        mixinStandardHelpOptions = true
+)
+
+public class App implements Callable<Integer> {
+    @Parameters(index = "0", paramLabel = "filepath1",
+    description = "path to first file")
+    private String filepath1;
+    @Parameters(index = "1", paramLabel = "filepath1",
+            description = "path to second file")
+    private String filepath2;
+    @Option(names = {"-f", "--format"}, paramLabel = "format", defaultValue = "stylish",
+    description = "output format [default: stylish]")
+    private String format;
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int exitCode = new CommandLine(new App()).execute(args);
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        return 0;
     }
 }
