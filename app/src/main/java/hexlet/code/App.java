8fe678ec83cc9b5a9c43f1ea6ea78f9917120229
Yulia Baranova <yulia.gorelikova@yandex.ru>
2024-06-08 11:06:31 +0300

    add checkstyle and differ template

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -15,10 +15,10 @@ import java.util.concurrent.Callable;
 
 public class App implements Callable<Integer> {
     @Parameters(index = "0", paramLabel = "filepath1",
-    description = "path to first file")
+                description = "path to first file")
     private String filepath1;
     @Parameters(index = "1", paramLabel = "filepath1",
-            description = "path to second file")
+                description = "path to second file")
     private String filepath2;
     @Option(names = {"-f", "--format"}, paramLabel = "format", defaultValue = "stylish",
     description = "output format [default: stylish]")
---
+++ app/src/main/java/hexlet/code/Differ.java
@@ -0,0 +1,9 @@
+package hexlet.code;
+
+public class Differ {
+
+    public static String generate(String filepath1, String filepath2) {
+        var str = "";
+        return str;
+    }
+}
