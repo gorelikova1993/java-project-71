0ef3839f15efdc0b9b09db49fc6d59db62414737
Yulia Baranova <yulia.gorelikova@yandex.ru>
2024-06-26 17:37:55 +0300

    added tests

---
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -0,0 +1,30 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DifferTest {
+
+    @Test
+    void generate() throws IOException {
+        var actual = Differ.generate("src/test/resources/file1.json", "src/test/resources/file2.json", "json");
+        var result = readFile("src/test/resources/result.json");
+        assertEquals(actual, result);
+    }
+
+    public static String readFile(String filepath) throws IOException {
+        Path path = Paths.get(filepath);
+        Stream<String> lines = Files.lines(path);
+        String data = lines.collect(Collectors.joining(""));
+        lines.close();
+        return data;
+    }
+}
